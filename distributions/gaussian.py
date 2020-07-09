87492a26507f5e9c7b5ea5464a7a7256f779ebaf
Matteo <hypecoder@localhost.localdomain>
2020-07-09 20:32:42 +0200

    bug fixed

--- distributions/gaussian.py
+++ distributions/gaussian.py
@@ -4,42 +4,47 @@ from variables.random_variable import RandomVariable as RV
 from variables.continue_variable import ContinueVariable
 from sets.reals import Reals as R
 from scipy import integrate
-from math import inf
+from math import inf, pi, e
+from random import gauss
+
+
+def simdist(mu=0, sigma=1, size=100):
+    return [gauss(mu, sigma) for i in range(size)]
 
 
 class Gaussian(ContinueVariable):
 
-    def __init__(self, mu, teta):
+    def __init__(self, mu, sigma):
 
-        super().__init__(samplespace=R((np.NINF, np.Inf)))
+        super().__init__(samplespace=R((-inf, inf)))
         # Mean
         self.mu = mu
         # teta ∈ R+
-        assert teta >= 0, Exception()
+        assert sigma >= 0, Exception()
         # Standard deviation
-        self.teta = teta
+        self.sigma = sigma
 
-    def pdf(self, x):
-        return (1/((2*np.pi)**0.5)) * 1/self.teta * (np.e ** ((-1/2) * ((x - self.mu)/self.teta) ** 2))
+    def pdf(self, x, normalized=False):
+        return (1/((2*pi)**0.5)) * 1/self.sigma * (e ** ((-1/2) * ((x - self.mu)/self.sigma) ** 2))
 
     def cdf(self, x):
-        return integrate.quad(Gaussian.toNormal().pdf, -inf, (x - self.mu) / self.teta)
+        return integrate.quad(Gaussian.normal().pdf, -inf, (x - self.mu) / self.sigma)
 
     def ev(self):
         return self.mu
 
     def var(self):
-        return self.teta ** 2
+        return self.sigma ** 2
 
     def devstd(self):
-        return self.teta
+        return self.sigma
 
     @staticmethod
-    def tonorm():
+    def normal():
         return Gaussian(0,1)
 
     def isnorm(self):
-        return True if self.mu == 0 and self.teta == 1 else False
+        return True if self.mu == 0 and self.sigma == 1 else False
 
     def pdfshape(self, span, *args, **kwargs):
         # Plot mass probability function on a stick graphic
@@ -54,12 +59,20 @@ class Gaussian(ContinueVariable):
         super().evshape(span, *args, **kwargs)
 
     def __add__(self, other):
+
+        assert isinstance(other, (float, int, Gaussian)), Exception
+
         if isinstance(other, Gaussian):
             # X ~ G(u1,o1), Y ~ G(u2,o2) => (X+Y)~G(u1+u2,o1+o2)
-            return Gaussian(self.mu + other.mu, self.teta + other.teta)
+            return Gaussian(self.mu + other.mu, self.sigma + other.teta)
         # X ~ G(u,o), b ∈ R => (X+b)~G(u+b,o)
-        return Gaussian(self.mu + other, self.teta)
+        return Gaussian(self.mu + other, self.sigma)
 
     def __mul__(self, a):
+
+        assert isinstance(a, (float, int)), Exception
         # X ~ G(u,o), a ∈ R => (aX)~G(a*u,|a|o)
-        return Gaussian(a * self.mu, abs(a) * self.teta)
+        return Gaussian(a * self.mu, abs(a) * self.sigma)
+
+    def simulate(self, size=100):
+        return [gauss(self.mu, self.sigma) for i in range(size)]
